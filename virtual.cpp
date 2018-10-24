a3021351f432fbce494596fb8f319826b4bf8f86
trainedfornothing <manvvergujjar@gmail.com>
2018-10-25 01:55:34 +0530

    new data

---
+++ virtual.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std; 
+class ab{
+protected:
+int a;
+public:
+void virtual seta(int b){
+cout<<"\nClass ab Function\n";
+a=b;
+}
+int geta(){
+return a;}
+
+};
+
+
+
+class bc : public ab{
+protected:
+int b;
+public:
+void seta(int c){
+cout<<"\nClass bc Function\n";
+a=c;
+}
+void setb(int c){
+b=c;}
+int getb(){
+return b;}
+int geta(){
+return a;}
+};
+
+int main(){
+ab *o1;
+
+bc o2;
+
+o1=&o2;
+
+
+o1->seta(10);
+
+
+cout<<o1->geta()<<endl;
+
+return 0;
+}
